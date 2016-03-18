d95ae393679fe4f18868a90ed63e885c6292f1eb
Olexander Kutsenko <monte-fm@users.noreply.github.com>
2016-03-18 10:37:01 +0200

    Added WP user

--- configs/autostart.sh
+++ configs/autostart.sh
@@ -3,7 +3,11 @@ service mysql start
 service php5-fpm start
 service nginx start
 service ssh start
-mysql -uroot -proot -e 'create database wordpress'
+echo "create database wordpress" | mysql -uroot -proot
+echo "CREATE USER 'wp'@'%' IDENTIFIED BY 'K7jtHs9Qkr';" | mysql -uroot -proot
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wp'@'%';" | mysql -uroot -proot
+echo "FLUSH PRIVILEGES;" | mysql -uroot -proot
+
 cd /var/www/ && tar -zxvf latest.tar.gz
 rm /var/www/latest.tar.gz
 mv /root/autostart /root/autostart.sh
