b810baacc852f89921b29df217d149136e3657ed
Olexander Kutsenko <monte-fm@users.noreply.github.com>
2016-03-18 10:35:47 +0200

    Changed db user to 'wp'@'%'

--- configs/wp-config.php
+++ configs/wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'wp');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'K7jtHs9Qkr');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
