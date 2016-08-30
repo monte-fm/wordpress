4135a09841500a45cf520f64bfe8a6c576c8cd50
okutsenko <okutsenko@grossum.com>
2016-08-30 13:21:24 +0300

    php7

--- configs/autostart.sh
+++ configs/autostart.sh
@@ -3,9 +3,3 @@ service mysql start
 service php7.0-fpm start
 service nginx start
 service ssh start
-
-cd /var/www/ && tar -zxvf latest.tar.gz
-rm /var/www/latest.tar.gz
-mv /root/autostart /root/autostart.sh
-chown -R www-data:www-data /var/www/wordpress
-
